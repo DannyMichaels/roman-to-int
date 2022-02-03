062934b3e5050ed0b9a40ed76653be4e9f7360fb
Daniel Michael <itzdanielmichael@gmail.com>
2022-02-02 23:57:14 -0500

    Update romanToInt.js

--- romanToInt.js
+++ romanToInt.js
@@ -27,3 +27,33 @@ var romanToInt = function(s) {
     }
     return sum
 };
+
+
+var romanToInt = function(s) {
+    let sum = 0
+    
+    const digits = {
+        "IX": 9,
+        "XL": 40,
+        "IV": 4,
+        "XC": 90,
+        "CD": 400,
+        "CM": 900,
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1,
+    }
+    
+    
+    for (const [romanNumeral, integerEquivalent] of Object.entries(digits)) {
+        while(s.includes(romanNumeral)) {
+            sum += integerEquivalent;
+            s = s.replace(romanNumeral, "")
+        }
+    }
+    return sum
+};
